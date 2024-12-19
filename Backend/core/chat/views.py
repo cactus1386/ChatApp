1743eb4b1cfc80169d1c24e3564b4fda1933a501
cactus1386 <cactusinjast@gmail.com>
2024-12-20 00:51:31 +0330

    add HomePage to project

--- Backend/core/chat/views.py
+++ Backend/core/chat/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Chat
 
 # Create your views here.
+
+
+def chat_view(request):
+    massages = Chat.objects.all()
+    return render(request, 'chat/chat.html', {'massages': massages})
+
+
+def home_view(request):
+    return render(request, 'home.html')
